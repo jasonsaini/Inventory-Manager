4b0102f4d9316a13658532c2a6b2ad95a9e81056
jasonsaini <jasonsaini.exe@gmail.com>
2021-07-21 17:17:32 -0400

    Added all controllers to UML diagram

--- src/main/java/ucf/assignments/InventoryManager.java
+++ src/main/java/ucf/assignments/InventoryManager.java
@@ -5,12 +5,9 @@
 package ucf.assignments;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class InventoryManager extends Application{
     public static void main(String args[])
