39c363fb91bb491d611bb1f3d5a6127937bdc952
jasonsaini <jasonsaini.exe@gmail.com>
2021-07-23 12:06:03 -0400

    Added name and serial number search features.

--- src/main/java/ucf/assignments/NameSearchController.java
+++ src/main/java/ucf/assignments/NameSearchController.java
@@ -4,5 +4,48 @@
  */
 package ucf.assignments;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
 public class NameSearchController {
+    @FXML
+    ObservableList<InventoryItem>  searchList = FXCollections.observableArrayList();
+    @FXML
+    TableView nameSearchTable;
+    @FXML
+    TableColumn nameSearchNameColumn, nameSearchValueColumn, nameSearchSNColumn;
+    @FXML
+    TextField nameSearchTextField;
+    public void nameSearchItemButtonClicked(ActionEvent actionEvent) {
+        nameSearchTable.getItems().clear();
+        searchList.clear();
+        Node node = (Node)actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        ObservableList<InventoryItem> fullInventoryList = (ObservableList<InventoryItem>) stage.getUserData();
+
+        String nameSearchKey = nameSearchTextField.getText();
+        for(int i = 0; i < fullInventoryList.size(); i++)
+        {
+            if(fullInventoryList.get(i).name.toLowerCase().contains(nameSearchKey.toLowerCase()))
+            {
+                searchList.add(fullInventoryList.get(i));
+            }
+        }
+        // set cell value factories
+        nameSearchNameColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("name"));
+        nameSearchValueColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("dollarVal"));
+        nameSearchSNColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("serialNum"));
+
+        nameSearchTable.setItems(searchList);
+        nameSearchTable.refresh();
+
+    }
 }
--- src/main/java/ucf/assignments/SerialNumberSearchController.java
+++ src/main/java/ucf/assignments/SerialNumberSearchController.java
@@ -4,5 +4,52 @@
  */
 package ucf.assignments;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
 public class SerialNumberSearchController {
+    @FXML
+    ObservableList<InventoryItem> searchList = FXCollections.observableArrayList();
+
+    @FXML
+    TableView SNSearchTable;
+
+    @FXML
+    TableColumn SNSearchNameColumn, SNValueColumn, SNSearchSNColumn;
+
+    @FXML
+    TextField searchSNTextField;
+
+    public void serialNumberSearchItemButtonClicked(ActionEvent actionEvent) {
+        SNSearchTable.getItems().clear();
+        searchList.clear();
+        Node node = (Node)actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        ObservableList<InventoryItem> fullInventoryList = (ObservableList<InventoryItem>) stage.getUserData();
+
+        String serialNumberSearchKey = searchSNTextField.getText().toUpperCase();
+        for(int i = 0; i < fullInventoryList.size(); i++)
+        {
+            if(fullInventoryList.get(i).serialNum.contains(serialNumberSearchKey))
+            {
+                searchList.add(fullInventoryList.get(i));
+            }
+        }
+
+        // set cell value factories
+        SNSearchNameColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("name"));
+        SNValueColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("dollarVal"));
+        SNSearchSNColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("serialNum"));
+
+        SNSearchTable.setItems(searchList);
+        SNSearchTable.refresh();
+    }
 }
